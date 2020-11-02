624772c7142cc2abe656812f8e6b286fce1b8742
Boyan-Apostolov <boian4934@gmail.com>
2020-11-02 15:41:35 +0200

    Implement TownSeeder

---
+++ Data/OnlineDoctorSystem.Data/Seeding/TownsSeeder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Internal;
+using OnlineDoctorSystem.Data.Models;
+
+namespace OnlineDoctorSystem.Data.Seeding
+{
+    public class TownsSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Towns.Any())
+            {
+                return;
+            }
+            var townNames = new string[]
+            {
+                "Благоевград",
+                "Бургас",
+                "Варна",
+                "Велико Търново",
+                "Враца",
+                "Габрово",
+                "Добрич",
+                "Кърджали",
+                "Кюстендил",
+                "Монтана",
+                "Пазарджик",
+                "Перник",
+                "Плевен",
+                "Пловдив",
+                "Разград",
+                "Русе",
+                "Силистра",
+                "Сливен",
+                "Смолян",
+                "София град",
+                "София област",
+                "Стара Загора",
+                "Търговище",
+                "Хасково",
+                "Шумен",
+                "Ямбол"
+            };
+            foreach (var townName in townNames)
+            {
+                await dbContext.Towns.AddAsync(new Town() { TownName = townName });
+            }
+        }
+    }
+}
