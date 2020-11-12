298479e3adc1de01cde59d37f31203c26f7f1535
Boyan-Apostolov <boian4934@gmail.com>
2020-11-12 19:20:35 +0200

    Add Validation of Consultation Time

--- Web/OnlineDoctorSystem.Web.ViewModels/Consultaions/AddConsultationViewModel.cs
+++ Web/OnlineDoctorSystem.Web.ViewModels/Consultaions/AddConsultationViewModel.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
+﻿using OnlineDoctorSystem.Web.ViewModels.Utilities;
 
 namespace OnlineDoctorSystem.Web.ViewModels.Consultaions
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+
     public class AddConsultationViewModel
     {
         public string DoctorId { get; set; }
@@ -15,10 +17,12 @@ namespace OnlineDoctorSystem.Web.ViewModels.Consultaions
 
         [Required(ErrorMessage = "Моля въведете начален час на консултацията")]
         [DataType(DataType.Date)]
+        [StartEndTimeValidation]
         public TimeSpan StartTime { get; set; }
 
         [Required(ErrorMessage = "Моля въведете краен час")]
         [DataType(DataType.Date)]
+        [StartEndTimeValidation]
         public TimeSpan EndTime { get; set; }
 
         public DateTime ReceivedOn => DateTime.UtcNow;
